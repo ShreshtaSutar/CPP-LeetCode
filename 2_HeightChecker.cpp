6cfda4bf3220dd3ff1f19b18aab5d4b654571460
Shreshta Sutar <139339969+Baymax155@users.noreply.github.com>
2025-02-14 21:49:34 +0530

    Create 2_HeightChecker.cpp

---
+++ 2_HeightChecker.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sorted_heights = heights;
+        int num = 0;
+        
+        sort(sorted_heights.begin(), sorted_heights.end());
+        for(int i = 0; i < heights.size(); i++){
+            if(heights[i] != sorted_heights[i]) num++;
+        }
+        
+        return num;
+    }
+};
