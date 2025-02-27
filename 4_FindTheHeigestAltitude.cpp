4d9f058406e9efc432fad0b13aea7388f5b57150
Shreshta Sutar <139339969+Baymax155@users.noreply.github.com>
2025-02-27 17:40:04 +0530

    new changes

---
+++ 4_FindTheHeigestAltitude.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int largestAltitude(vector <int>& gain) {
+        int ans = 0, h = 0;
+        for (int v : gain) h += v, ans = max(ans, h);
+        return ans;
+    }
+};
+
