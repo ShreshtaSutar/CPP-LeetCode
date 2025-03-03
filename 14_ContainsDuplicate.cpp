f038c941807c6e53c5346aab4782a66d06194479
Shreshta Sutar <139339969+Baymax155@users.noreply.github.com>
2025-03-03 22:19:34 +0530

    Update 14_ContainsDuplicate.cpp

---
+++ 14_ContainsDuplicate.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+
+class Solution {
+    public:
+        bool containsDuplicate(vector<int>& nums) {
+            unordered_set<int> nums_set(nums.begin(), nums.end());
+            return nums_set.size() != nums.size();
+        }
+    };
