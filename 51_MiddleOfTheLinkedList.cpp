52e04de938ecb03c335148360bec92716c7c4e88
Shreshta Sutar <139339969+ShreshtaSutar@users.noreply.github.com>
2025-04-05 22:24:03 +0530

    Update 51_MiddleOfTheLinkedList.cpp

---
+++ 51_MiddleOfTheLinkedList.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+class Solution {
+    public:
+        ListNode* middleNode(ListNode* head) {
+            ListNode *slow = head, *fast = head;
+            while (fast && fast->next) {
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            return slow;
+        }
+    };
